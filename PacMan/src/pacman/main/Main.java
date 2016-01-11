ae651a0e60c22985e77ef7a6e696b51fdc29fce9
Evan Miller <eamiller@dhcp-asv-100.helsinki.fi>
2016-01-11 16:30:50 +0200

    siistimistä

--- PacMan/src/pacman/main/Main.java
+++ PacMan/src/pacman/main/Main.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication5;
+package pacman.main;
 
 /**
  *
  * @author eamiller
  */
-public class JavaApplication5 {
+public class Main {
 
     /**
      * @param args the command line arguments
