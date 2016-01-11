74e2c0771d4299b638f477ab33a494178edfc3e5
Evan Miller <eamiller@dhcp-asv-100.helsinki.fi>
2016-01-11 16:29:17 +0200

    netBeans

--- PacMan/src/javaapplication5/JavaApplication5.java
+++ PacMan/src/javaapplication5/JavaApplication5.java
@@ -3,12 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package kingscup.pacman;
+package javaapplication5;
 
 /**
  *
  * @author eamiller
  */
-public class Main {
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
